d31478930a169c8bc843e1922f4da2c4b2e0d467
iloktionov <iloktionov@skbkontur.ru>
2013-03-03 15:39:19 +0600

    Node: marked serializable + equality members.

--- Core/MasterFakeNode.cs
+++ Core/MasterFakeNode.cs
@@ -5,6 +5,7 @@ namespace Dixie.Core
 	/// <summary>
 	/// Тип ноды, обозначающий место Мастера в топологии грида.  
 	/// </summary>
+	[Serializable]
 	internal class MasterFakeNode : INode
 	{
 		public Guid Id
--- Core/Node.cs
+++ Core/Node.cs
@@ -2,6 +2,7 @@
 
 namespace Dixie.Core
 {
+	[Serializable]
 	public class Node : INode
 	{
 		public Node(double performance, double failureProbability)
@@ -14,5 +15,25 @@ namespace Dixie.Core
 		public Guid Id { get; private set; }
 		public Double Performance { get; private set; }
 		public Double FailureProbability { get; private set; }
+
+		#region Equality members
+		protected bool Equals(Node other)
+		{
+			return Id.Equals(other.Id);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((Node)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode();
+		} 
+		#endregion
 	}
 }
