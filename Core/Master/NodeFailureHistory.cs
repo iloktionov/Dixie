c57f8f9060dd7b0cbd262af1263263e85c19a10c
iloktionov <iloktionov@skbkontur.ru>
2013-03-13 20:51:48 +0600

    + NodeFailureHistory.

---
+++ Core/Master/NodeFailureHistory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dixie.Core
+{
+	public class NodeFailureHistory
+	{
+		public NodeFailureHistory()
+		{
+			FailureDurations = new List<TimeSpan>();
+		}
+
+		public bool HasFailures()
+		{
+			return FailureDurations.Any();
+		}
+
+		public TimeSpan MaxFailureTime()
+		{
+			return FailureDurations.Max();
+		}
+
+		public TimeSpan MinFailureTime()
+		{
+			return FailureDurations.Min();
+		}
+
+		internal void AddFailure(TimeSpan failureDuration)
+		{
+			FailureDurations.Add(failureDuration);
+		}
+
+		public List<TimeSpan> FailureDurations { get; private set; }
+	}
+}
