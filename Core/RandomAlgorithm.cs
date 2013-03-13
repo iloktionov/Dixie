415f2cc9d56526b0591dc7db5cce62d65f7a1c34
iloktionov <iloktionov@skbkontur.ru>
2013-03-13 17:47:33 +0600

    Simplest random algorithm.

---
+++ Core/RandomAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dixie.Core
+{
+	internal class RandomAlgorithm : ISchedulerAlgorithm
+	{
+		public RandomAlgorithm(Random random)
+		{
+			this.random = random;
+		}
+
+		public RandomAlgorithm()
+			: this (new Random()) { }
+
+		public void Work(List<NodeInfo> aliveNodes, TaskManager taskManager)
+		{
+			foreach (Task pendingTask in taskManager.GetPendingTasks())
+				taskManager.AssignNodeToTask(pendingTask, aliveNodes[random.Next(aliveNodes.Count)].Id);
+		}
+
+		private readonly Random random;
+	}
+}
