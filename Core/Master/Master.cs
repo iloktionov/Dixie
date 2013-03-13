c61d0d0b3e06c611fb33ede24e547e8fbfebf685
iloktionov <iloktionov@skbkontur.ru>
2013-03-13 12:54:42 +0600

    Started to work on master.

---
+++ Core/Master/Master.cs
@@ -0,0 +1,19 @@
+﻿namespace Dixie.Core
+{
+	internal class Master
+	{
+		public Master()
+		{
+			nodesManager = new NodesManager();
+			syncObject = new object();
+		}
+
+		public HeartBeatResponse HandleHeartBeatMessage(HeartBeatMessage message)
+		{
+			return null;
+		}
+
+		private readonly NodesManager nodesManager;
+		private readonly object syncObject;
+	}
+}
---
+++ Core/Master/NodesManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Dixie.Core
+{
+	internal class NodesManager
+	{
+		public NodesManager()
+		{
+			watch = Stopwatch.StartNew();
+		}
+
+		public void HandleHeartBeatMessage(HeartBeatMessage message)
+		{
+		}
+
+		public void UpdateState()
+		{
+			
+		}
+
+		private readonly TimeSpan deadabilityThreshold;
+		private readonly Stopwatch watch;
+	}
+}
