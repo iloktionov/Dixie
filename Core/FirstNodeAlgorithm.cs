ed9ab56020eb4677e1645b5033110103feaf3d24
iloktionov <iloktionov@skbkontur.ru>
2013-03-13 20:20:50 +0600

    Another very poor algorithm for test purposes.

---
+++ Core/FirstNodeAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Dixie.Core
+{
+	internal class FirstNodeAlgorithm : ISchedulerAlgorithm
+	{
+		public FirstNodeAlgorithm()
+		{
+			Name = "FirstNodeAlgorithm";
+		}
+
+		public void Work(List<NodeInfo> aliveNodes, TaskManager taskManager)
+		{
+			if (aliveNodes.Count <= 0)
+				return;
+			foreach (Task pendingTask in taskManager.GetPendingTasks())
+				taskManager.AssignNodeToTask(pendingTask, aliveNodes[0].Id);
+		}
+
+		public string Name { get; private set; }
+	}
+}
