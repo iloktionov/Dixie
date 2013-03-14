e8c3070960f47c687cac197f4dd1c3920711bd8b
iloktionov <iloktionov@skbkontur.ru>
2013-03-14 12:49:48 +0600

    InitialGridState: serialization and factory methods.

--- Core/Config/InitialGridState.cs
+++ Core/Config/InitialGridState.cs
@@ -1,5 +1,10 @@
-﻿namespace Dixie.Core
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Dixie.Core
 {
+	[Serializable]
 	public class InitialGridState
 	{
 		public InitialGridState(Topology topology, int randomSeed, TopologySettings topologySettings, EngineSettings engineSettings)
@@ -8,6 +13,43 @@
 			TopologySettings = topologySettings;
 			Topology = topology;
 			RandomSeed = randomSeed;
+		}
+
+		public void Serialize(Stream stream)
+		{
+			new BinaryFormatter().Serialize(stream, this);
+		}
+
+		public void SaveToFile(string file)
+		{
+			using (var stream = new FileStream(file, FileMode.Create, FileAccess.Write))
+				Serialize(stream);
+		}
+
+		public static InitialGridState Deserialize(Stream stream)
+		{
+			return (InitialGridState) new BinaryFormatter().Deserialize(stream);
+		}
+
+		public static InitialGridState ReadFromFile(string file)
+		{
+			using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read))
+				return Deserialize(stream);
+		}
+
+		public static InitialGridState GenerateNew(int nodesCount, Random random, TopologySettings topologySettings, EngineSettings engineSettings)
+		{
+			return new InitialGridState(new TopologyBuilder(random).Build(nodesCount), random.Next(), topologySettings, engineSettings);
+		}
+
+		public static InitialGridState GenerateNew(int nodesCount, Random random)
+		{
+			return GenerateNew(nodesCount, random, TopologySettings.GetInstance(), EngineSettings.GetInstance());
+		}
+
+		public static InitialGridState GenerateNew(int nodesCount)
+		{
+			return GenerateNew(nodesCount, new Random());
 		}
 
 		public Topology Topology { get; private set; }
