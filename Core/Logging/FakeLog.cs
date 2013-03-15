4697fa94fc9f2cc838daccd85d16d1c1570db43e
iloktionov <iloktionov@skbkontur.ru>
2013-03-16 02:21:40 +0600

    Logging: + FakeLog.

---
+++ Core/Logging/FakeLog.cs
@@ -0,0 +1,15 @@
+﻿namespace Dixie.Core
+{
+	public class FakeLog : ILog
+	{
+		public void Debug(string format, params object[] args) { }
+
+		public void Info(string format, params object[] args) { }
+
+		public void Warn(string format, params object[] args) { }
+
+		public void Error(string format, params object[] args) { }
+
+		public bool IsDebugEnabled { get; set; }
+	}
+}
