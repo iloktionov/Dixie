76be0072b3b80734fe256634295f3cec39c64d74
iloktionov <iloktionov@skbkontur.ru>
2013-03-16 17:49:46 +0600

    DixieModel: INotifyPropertyChanged.

--- Dixie.Presentation/DixieModel.cs
+++ Dixie.Presentation/DixieModel.cs
@@ -1,7 +1,27 @@
-﻿namespace Dixie.Presentation
+﻿using System.ComponentModel;
+
+namespace Dixie.Presentation
 {
-	internal class DixieModel
+	internal class DixieModel : INotifyPropertyChanged
 	{
-		public DixieGraph TopologyGraph { get; set; }
+		public DixieGraph TopologyGraph
+		{
+			get { return topologyGraph; }
+			set
+			{
+				topologyGraph = value;
+				RaisePropertyChanged("TopologyGraph");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void RaisePropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+		}
+
+		private DixieGraph topologyGraph;
 	}
 }
