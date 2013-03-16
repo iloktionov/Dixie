fc3f8158bd7834a3ccd84ca9a5f3d199a6238722
iloktionov <iloktionov@skbkontur.ru>
2013-03-16 21:23:57 +0600

    AddNodesMutator: prefer to add new nodes to unpopulated parents.

--- Dixie.Core/Mutation/AddNodesMutator.cs
+++ Dixie.Core/Mutation/AddNodesMutator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Dixie.Core
@@ -20,7 +21,7 @@ namespace Dixie.Core
 			if (topology.WorkerNodesCount >= maxRemainingNodes)
 				return;
 			int nodesToAdd = random.Next(0, maxRemainingNodes - topology.WorkerNodesCount + 1);
-			INode[] parents = SelectRandomParents(topology, nodesToAdd);
+			INode[] parents = SelectParents(topology, nodesToAdd);
 			for (int i = 0; i < nodesToAdd; i++)
 			{
 				var newNode = new Node(configurator.GeneratePerformance(), configurator.GenerateFailureProbability(), NodeFailurePattern.Generate(random));
@@ -28,13 +29,29 @@ namespace Dixie.Core
 			}
 		}
 
-		private INode[] SelectRandomParents(Topology topology, int count)
+		private static INode[] SelectParents(Topology topology, int count)
 		{
-			var allNodesList = topology.Graph.Vertices.ToArray();
+			var potentialParents = topology.Graph.Vertices
+				.Select(vertex => new KeyValuePair<INode, int>(vertex, GetTreeChildrenCount(topology, vertex)))
+				.OrderBy(pair => pair.Value)
+				.ToList();
 			var result = new INode[count];
+			int parentIndex = 0;
 			for (int i = 0; i < count; i++)
-				result[i] = allNodesList[random.Next(0, allNodesList.Length)];
+			{
+				result[i] = potentialParents[parentIndex].Key;
+				parentIndex++;
+				parentIndex = parentIndex % potentialParents.Count;
+			}
 			return result;
+		}
+
+		private static int GetTreeChildrenCount(Topology topology, INode vertex)
+		{
+			IEnumerable<NetworkLink> inEdges;
+			if (!topology.Graph.TryGetInEdges(vertex, out inEdges))
+				return 0;
+			return inEdges.Count();
 		}
 
 		private readonly Random random;
