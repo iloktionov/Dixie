2ab10916ee2ba180952176b7ab1a434b634706b6
iloktionov <iloktionov@skbkontur.ru>
2013-03-17 01:19:01 +0600

    Updated deploy script.

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,9 @@
 del /F /S /Q Deploy\* 
-xcopy Dixie.Console\bin\* Deploy\ /Y
+xcopy Dixie.Presentation\bin\*.exe Deploy\ /Y
+xcopy Dixie.Presentation\bin\*.dll Deploy\ /Y
+xcopy Dixie.Presentation\bin\*.pdb Deploy\ /Y
+xcopy Dixie.Presentation\bin\Dixie.Presentation.exe.config Deploy\ /Y
+xcopy Dixie.Console\bin\Dixie.Console* Deploy\ /Y
 xcopy Dixie.Algorithms\bin\Dixie.Algorithms.dll Deploy\Algorithms\ /Y
 xcopy Dixie.Algorithms\bin\Dixie.Algorithms.pdb Deploy\Algorithms\ /Y
 
