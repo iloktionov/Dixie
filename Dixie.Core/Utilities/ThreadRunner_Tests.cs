13869868c0418fa6b1615bc8daa58d600c58dd9c
iloktionov <iloktionov@skbkontur.ru>
2013-03-17 16:27:09 +0600

    Test for ThreadRunner.

---
+++ Dixie.Core/Utilities/ThreadRunner_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace Dixie.Core
+{
+	[TestFixture]
+	internal class ThreadRunner_Tests
+	{
+		[Test]
+		[Timeout(5000)]
+		public void Test_StopThreads()
+		{
+			var thread = ThreadRunner.Run(() => Thread.Sleep(Timeout.Infinite));
+			ThreadRunner.StopThreads(thread);
+			ThreadRunner.StopThreads(thread);
+			ThreadRunner.StopThreads(thread);
+		}
+	}
+}
