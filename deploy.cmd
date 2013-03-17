1b78508376b2e8954f8b01069e4ac70284abbb0f
iloktionov <iloktionov@skbkontur.ru>
2013-03-17 22:31:29 +0600

    deploy.cmd: include config examples in deploy.

--- deploy.cmd
+++ deploy.cmd
@@ -3,6 +3,8 @@ xcopy Dixie.Presentation\bin\*.exe Deploy\ /Y
 xcopy Dixie.Presentation\bin\*.dll Deploy\ /Y
 xcopy Dixie.Presentation\bin\*.pdb Deploy\ /Y
 xcopy Dixie.Presentation\bin\Dixie.Presentation.exe.config Deploy\ /Y
+xcopy Dixie.Presentation\bin\*.example Deploy\ /Y
+xcopy Dixie.Core\bin\*.example Deploy\ /Y
 xcopy Dixie.Console\bin\Dixie.Console* Deploy\ /Y
 xcopy Dixie.Algorithms\bin\Dixie.Algorithms.dll Deploy\Algorithms\ /Y
 xcopy Dixie.Algorithms\bin\Dixie.Algorithms.pdb Deploy\Algorithms\ /Y
