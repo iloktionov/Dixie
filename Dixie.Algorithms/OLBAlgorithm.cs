1ba281064b5045a3d271ac32d204dc8a005dfa36
iloktionov <iloktionov@skbkontur.ru>
2013-04-01 18:48:03 +0600

    + OLBAlgorithm

---
+++ Dixie.Algorithms/OLBAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace Dixie.Core
+{
+	// (iloktionov): Opportunistic load balancing algorithm.
+	[Export(typeof(ISchedulerAlgorithm))]
+	internal class OLBAlgorithm : ISchedulerAlgorithm
+	{
+		public OLBAlgorithm(string name)
+		{
+			Name = name;
+		}
+
+		public OLBAlgorithm()
+			: this("OLBAlgorithm") { }
+
+		public IEnumerable<TaskAssignation> AssignNodes(List<NodeInfo> aliveNodes, List<Task> pendingTasks)
+		{
+			var assignations = new List<TaskAssignation>(pendingTasks.Count);
+			var freeNodesPool = new Queue<NodeInfo>(aliveNodes.Where(info => info.AvailabilityTime.Equals(TimeSpan.Zero)));
+
+			for (int i = 0; i < pendingTasks.Count; i++)
+			{
+				if (freeNodesPool.Count <= 0)
+					return assignations;
+				assignations.Add(new TaskAssignation(pendingTasks[i], freeNodesPool.Dequeue().Id));
+			}
+			return assignations;
+		}
+
+		public void Reset() { }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+
+		public string Name { get; set; }
+	}
+}
