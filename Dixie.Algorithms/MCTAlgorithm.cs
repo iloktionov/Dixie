7638844184cf2f8c93ad19561c8c816d694994a7
iloktionov <iloktionov@skbkontur.ru>
2013-04-01 18:02:33 +0600

    Dixie.Algorithms: started to implement MCTAlgorithm.

---
+++ Dixie.Algorithms/MCTAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace Dixie.Core
+{
+	[Export(typeof(ISchedulerAlgorithm))]
+	internal class MCTAlgorithm : ISchedulerAlgorithm
+	{
+		public MCTAlgorithm(string name)
+		{
+			Name = name;
+		}
+
+		public MCTAlgorithm()
+			: this("MCTAlgorithm") { }
+
+		public IEnumerable<TaskAssignation> AssignNodes(List<NodeInfo> aliveNodes, List<Task> pendingTasks)
+		{
+			Double[,] etcMatrix = ConstructETCMatrix(aliveNodes, pendingTasks);
+			throw new System.NotImplementedException();
+		}
+
+		public void Reset() { }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+
+		public string Name { get; set; }
+
+		// (iloktionov): Элемент в позиции (i, j) соответствует времени выполнения i-го задания j-й машиной.
+		protected Double[,] ConstructETCMatrix(List<NodeInfo> aliveNodes, List<Task> pendingTasks)
+		{
+			var etcMatrix = new Double[pendingTasks.Count, aliveNodes.Count];
+			for (int i = 0; i < pendingTasks.Count; i++)
+				for (int j = 0; j < aliveNodes.Count; j++)
+					etcMatrix[i, j] = pendingTasks[i].Volume / aliveNodes[j].Performance;
+			return etcMatrix;
+		}
+	}
+}
