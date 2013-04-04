80778abaf159df4a7b12a5ebcef1404a74ea8633
iloktionov <iloktionov@skbkontur.ru>
2013-04-04 14:46:50 +0600

    Draw plot legend outside of actual plot.

--- Dixie.Presentation/Plotting/PlotModelFactory.cs
+++ Dixie.Presentation/Plotting/PlotModelFactory.cs
@@ -10,6 +10,7 @@ namespace Dixie.Presentation
 			var plotModel = new PlotModel("Total work done");
 			plotModel.Axes.Add(new LinearAxis(AxisPosition.Left, Double.NaN, Double.NaN, "Work units"));
 			plotModel.Axes.Add(new LinearAxis(AxisPosition.Bottom, Double.NaN, Double.NaN, "Time, sec"));
+			plotModel.LegendPlacement = LegendPlacement.Outside;
 			return plotModel;
 		}
 	}
