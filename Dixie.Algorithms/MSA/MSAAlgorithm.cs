0cd10b16869f41c586e299efa1a45f548c795f39
iloktionov <iloktionov@skbkontur.ru>
2013-04-22 23:47:43 +0600

    rollback to initial solution if there are too few tasks

--- Dixie.Algorithms/MSA/MSAAlgorithm.cs
+++ Dixie.Algorithms/MSA/MSAAlgorithm.cs
@@ -5,6 +5,11 @@ using System.Linq;
 
 namespace Dixie.Core
 {
+	// (iloktionov): Makespan решения, мутирующего из initialSolution можно не считать каждый раз.
+	// Достаточно один раз запомнить все completionTimes в отсортированном виде.
+
+	// (iloktionov): Makespan решения, получающегося из current, необходимо полностью пересчитывать лишь тогда, когда перестановка затрагивает худшую ноду.
+
 	[Export(typeof(ISchedulerAlgorithm))]
 	internal partial class MSAAlgorithm : ISchedulerAlgorithm
 	{
@@ -29,6 +34,8 @@ namespace Dixie.Core
 			Double[,] etcMatrix = MatricesHelper.ConstructETCMatrix(aliveNodes, pendingTasks);
 			Double[] availabilityVector = MatricesHelper.ConstructAvailabilityVector(aliveNodes);
 			Int32[] initialSolution = new MCTAlgorithm(etcMatrix).AssignNodesInternal(aliveNodes, pendingTasks);
+			if (pendingTasks.Count < aliveNodes.Count / 4)
+				return ConvertSolution(initialSolution, aliveNodes, pendingTasks);
 
 			Int32[] bestSolution = initialSolution;
 			Double bestMakespan = MakespanCalculator.Calculate(initialSolution, etcMatrix, availabilityVector);
@@ -63,9 +70,7 @@ namespace Dixie.Core
 			}
 			log.Info("Best solution makespan: {0:0.00000}", bestMakespan);
 
-			return bestSolution
-				.Select((nodeIdx, taskIdx) => new TaskAssignation(pendingTasks[taskIdx], aliveNodes[nodeIdx].Id))
-				.ToList();
+			return ConvertSolution(bestSolution, aliveNodes, pendingTasks);
 		}
 
 		public virtual void Reset() { }
@@ -76,6 +81,13 @@ namespace Dixie.Core
 		}
 
 		public string Name { get; set; }
+
+		private static IEnumerable<TaskAssignation> ConvertSolution(IEnumerable<Int32> solution, List<NodeInfo> aliveNodes, List<Task> pendingTasks)
+		{
+			return solution
+				.Select((nodeIdx, taskIdx) => new TaskAssignation(pendingTasks[taskIdx], aliveNodes[nodeIdx].Id))
+				.ToList();
+		} 
 
 		private Int32[] CloneWithExchange(Int32[] initialSolution)
 		{
