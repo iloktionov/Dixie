f0acce660b48cdc62c6b668e7ac9f0cec3f22d0d
iloktionov <iloktionov@skbkontur.ru>
2013-04-23 18:30:29 +0600

    Dixie.Presentation: russian texts on plot.

--- Dixie.Presentation/Plotting/PlotModelFactory.cs
+++ Dixie.Presentation/Plotting/PlotModelFactory.cs
@@ -7,9 +7,9 @@ namespace Dixie.Presentation
 	{
 		public static PlotModel CreateEmpty()
 		{
-			var plotModel = new PlotModel("Total work done");
-			plotModel.Axes.Add(new LinearAxis(AxisPosition.Left, Double.NaN, Double.NaN, "Work units"));
-			plotModel.Axes.Add(new LinearAxis(AxisPosition.Bottom, Double.NaN, Double.NaN, "Time, sec"));
+			var plotModel = new PlotModel("Всего выполнено работы");
+			plotModel.Axes.Add(new LinearAxis(AxisPosition.Left, Double.NaN, Double.NaN, "Единицы работы"));
+			plotModel.Axes.Add(new LinearAxis(AxisPosition.Bottom, Double.NaN, Double.NaN, "Время, сек."));
 			plotModel.LegendPlacement = LegendPlacement.Outside;
 			return plotModel;
 		}
